43af5af3fb521eaf5f359aa3f0cc5cd31b59f768
Sean Kladek <sean@303software.com>
2017-06-09 10:14:46 -0600

    Pod install

--- Pods/Target Support Files/Pods-SKTableViewDataSourceTests/Pods-SKTableViewDataSourceTests-umbrella.h
+++ Pods/Target Support Files/Pods-SKTableViewDataSourceTests/Pods-SKTableViewDataSourceTests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_TableViewDataSourceTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TableViewDataSourceTestsVersionString[];
+FOUNDATION_EXPORT double Pods_SKTableViewDataSourceTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SKTableViewDataSourceTestsVersionString[];
 
