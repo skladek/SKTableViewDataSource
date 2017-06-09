f8d843263f14dfc191a9f50dd38c3667dfe3b6dd
Sean Kladek <sean@303software.com>
2017-06-09 10:37:15 -0600

    Rebuilt the project file to fix pod linking errors

---
+++ SKTableViewDataSource/SKTableViewDataSource.h
@@ -0,0 +1,19 @@
+//
+//  SKTableViewDataSource.h
+//  SKTableViewDataSource
+//
+//  Created by Sean on 6/9/17.
+//  Copyright © 2017 Sean Kladek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKTableViewDataSource.
+FOUNDATION_EXPORT double SKTableViewDataSourceVersionNumber;
+
+//! Project version string for SKTableViewDataSource.
+FOUNDATION_EXPORT const unsigned char SKTableViewDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKTableViewDataSource/PublicHeader.h>
+
+
